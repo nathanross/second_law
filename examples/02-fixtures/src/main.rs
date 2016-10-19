59ca63b362891dc1b6d0dd58953c289453e563d2
Nathan Ross <nross.se@gmail.com>
2016-10-19 01:34:56 -0400

    mv to -f flag

--- examples/02-fixtures/src/main.rs
+++ examples/02-fixtures/src/main.rs
@@ -8,28 +8,38 @@ fn failure(reason: String) {
     exit(1);
 }
 
-fn main() {
-    let args : Vec<String> = std::env::args().skip(1).collect();
+fn average(sample : &[String]) -> f64 {
     let mut summed : u64 = 0;
-    let pathb = PathBuf::from(args.get(0).unwrap());
-    let mut contents = String::new();
-    File::open(pathb).unwrap().read_to_string(&mut contents);
-    let mut num_lines : u64 = 0;
-    for (line_num, line_text) in contents.split("\n").enumerate() {
-        if line_text != "" {
-            match line_text.parse::<u32>() {
-                Ok(num) => {
-                    summed += num as u64;
-                    num_lines += 1;
-                },
-                _ => {
-                    failure(format!("could not parse the text '{0}' as an integer at line {1}", line_text, line_num));
-                }
+    let mut datapoints : u64 = 0;
+    for (point_num, point_text) in sample.iter().enumerate() {        
+        match point_text.parse::<u32>() {
+            Ok(num) => {
+                summed += num as u64;
+                datapoints += 1;
+            },
+            _ => {
+                failure(format!("could not parse the text '{0}' as an integer at line {1}", point_text, point_num));
             }
         }
     }
-    let average = summed as f64 / num_lines as f64;
-    let rounded = average.round() as u64;
-    println!("{}", rounded.to_string());
+    return summed as f64 / datapoints as f64;
+}
+
+fn sample_from_file(filename: &str) -> Vec<String> {
+    let pathb = PathBuf::from(filename);
+    let mut contents = String::new();
+    File::open(pathb).unwrap().read_to_string(&mut contents).unwrap();
+    contents.split("\n").filter(|x| *x != "").map(|x| x.to_owned()).collect()
+}
+
+fn main() {
+    let args : Vec<String> = std::env::args().skip(1).collect();
+    let sample = if args.len() == 2 && *args.get(0).unwrap() == "-f" {
+        sample_from_file(args.get(1).unwrap())
+    } else {
+        args
+    };
+    let result = average(&sample).round() as u64;
+    println!("{}", result.to_string());
     exit(0);
 }
